4087442ad9fdf302a9e147c2b542ccaed36e14e2
bapuji5 <56861049+bapuji5@users.noreply.github.com>
2021-06-22 10:55:13 +0530

    Update smartBulb.ino

--- IoT-using-Blynk-Mobile-Platform/smartBulb.ino
+++ IoT-using-Blynk-Mobile-Platform/smartBulb.ino
@@ -1,1 +1,17 @@
-x
+#define BLYNK_PRINT Serial
+#include<ESP8266WiFi.h>
+#include<BlynkSimpleEsp8266.h>
+char ssid[] = "wifi name";//change this
+char pass[] = "wifi password";//change this
+char auth[] = "auth token";//change this
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Blynk.run();
+  }
