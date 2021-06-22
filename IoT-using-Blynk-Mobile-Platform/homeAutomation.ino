820b79f8e000856cf282ee25974b73c0c265aaab
bapuji5 <56861049+bapuji5@users.noreply.github.com>
2021-06-22 10:18:54 +0530

    Update homeAutomation.ino

--- IoT-using-Blynk-Mobile-Platform/homeAutomation.ino
+++ IoT-using-Blynk-Mobile-Platform/homeAutomation.ino
@@ -4,41 +4,14 @@
 char ssid[] = "wifi name";//change this
 char pass[] = "wifi password";//change this
 char auth[] = "auth token";//change this
-int x;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
-  pinMode(D2, OUTPUT); //red
-  pinMode(D3, OUTPUT); //green
-  pinMode(D4, OUTPUT); //blue
-}
-
-BLYNK_WRITE(V1)
-{
-  x = param.asInt();
-}
+  }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Blynk.run();
-  Serial.println(x);
-  if (x == 0) {
-    digitalWrite(D2, LOW);
-    digitalWrite(D3, LOW);
-    digitalWrite(D4, LOW);
   }
-  else if (x == 1) {
-    digitalWrite(D2, HIGH);
-    digitalWrite(D3, LOW);
-    digitalWrite(D4, HIGH);
-  }
-  else if (x == 2) {
-    digitalWrite(D2, LOW);
-    digitalWrite(D3, HIGH);
-    digitalWrite(D4, LOW);
-  }
-  else {
-    Serial.println("Invalid Input");
-  }
-}
