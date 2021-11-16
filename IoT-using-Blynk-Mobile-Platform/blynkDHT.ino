583042332df904bd52dd56205b3e0949efdca91e
bapuji5 <56861049+bapuji5@users.noreply.github.com>
2021-11-16 21:22:53 +0530

    Update blynkDHT.ino

--- IoT-using-Blynk-Mobile-Platform/blynkDHT.ino
+++ IoT-using-Blynk-Mobile-Platform/blynkDHT.ino
@@ -1,12 +1,21 @@
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID           "TMPLxxxxxx"
+#define BLYNK_DEVICE_NAME           "Device"
+#define BLYNK_AUTH_TOKEN            "YourAuthToken"
+// Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
+
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+
 #include <DHT.h>
 #define DHTPIN D1         
-#define DHTTYPE DHT11     
+#define DHTTYPE DHT11 
+char auth[] = BLYNK_AUTH_TOKEN;
+ 
 char ssid[]="wifi name";//Provide wifi name 
 char pass[]="wifi password";//Provide wifi Password
-char auth[]="Auth Token";//Provide Auth Token 
 
 DHT dht(DHTPIN, DHTTYPE);
 
