39aca06b034b0e9fde524eb3e73d91bdcd1909f0
bapuji5 <56861049+bapuji5@users.noreply.github.com>
2021-11-16 21:18:30 +0530

    Update smartBulb.ino

--- IoT-using-Blynk-Mobile-Platform/smartBulb.ino
+++ IoT-using-Blynk-Mobile-Platform/smartBulb.ino
@@ -1,17 +1,34 @@
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID           "TMPLxxxxxx"
+#define BLYNK_DEVICE_NAME           "Device"
+#define BLYNK_AUTH_TOKEN            "YourAuthToken"
+
+// Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
-#include<ESP8266WiFi.h>
-#include<BlynkSimpleEsp8266.h>
-char ssid[] = "wifi name";//change this
-char pass[] = "wifi password";//change this
-char auth[] = "auth token";//change this
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
-  }
+ 
+}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
   Blynk.run();
-  }
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
